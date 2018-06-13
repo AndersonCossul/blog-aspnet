74d6d77b65ed7a95c294155a7651ac7b103c95e7
Anderson Cossul <anderson_cossul@hotmail.com>
2018-06-13 03:07:41 -0300

    Cleanup, basic assets and StoreController.

---
+++ MusicStore/Controllers/StoreController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Browse(string genre)
+        {
+            return View();
+        }
+
+        public ActionResult Details(int ID)
+        {
+            return View();
+        }
+    }
+}
