c681e26f301ff33be72035c0305f9cdaaef9e714
Anderson Cossul <anderson_cossul@hotmail.com>
2018-06-13 03:35:17 -0300

    Updated models.

--- MusicStore/Models/Album.cs
+++ MusicStore/Models/Album.cs
@@ -7,7 +7,13 @@ namespace MusicStore.Models
 {
     public class Album
     {
-        public string Title { get; set; }
-        public Genre genre { get; set; }
+        public int      AlbumID { get; set; }
+        public int      GenreID { get; set; }
+        public int      ArtistID { get; set; }
+        public string   Title { get; set; }
+        public double   Price { get; set; }
+        public string   AlbumArtURL { get; set; }
+        public Genre    Genre { get; set; }
+        public Artist   Artist { get; set; }
     }
 }
