5474d8335fbe964a7eab020206251429c7080c40
Anderson Cossul <anderson_cossul@hotmail.com>
2018-06-13 23:32:52 -0300

    Adding Entity Framework

--- MusicStore/Global.asax.cs
+++ MusicStore/Global.asax.cs
@@ -12,6 +12,8 @@ namespace MusicStore
     {
         protected void Application_Start()
         {
+            System.Data.Entity.Database.SetInitializer(
+            new MusicStore.Models.SampleData());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
--- MusicStore/Models/Album.cs
+++ MusicStore/Models/Album.cs
@@ -11,8 +11,8 @@ namespace MusicStore.Models
         public int      GenreID { get; set; }
         public int      ArtistID { get; set; }
         public string   Title { get; set; }
-        public double   Price { get; set; }
-        public string   AlbumArtURL { get; set; }
+        public decimal  Price { get; set; }
+        public string   AlbumArtUrl { get; set; }
         public Genre    Genre { get; set; }
         public Artist   Artist { get; set; }
     }
